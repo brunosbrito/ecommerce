030a9231d288969c7662f7ad094ba7767069729d
Bruno <brunobrito728@gmail.com>
2024-05-05 17:14:31 -0300

    add item card

--- ecommerce-frontend/src/app/item-card/item-card.component.ts
+++ ecommerce-frontend/src/app/item-card/item-card.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-item-card',
   standalone: true,
@@ -12,7 +13,9 @@ import {MatCardModule} from '@angular/material/card';
 export class ItemCardComponent {
   @Input() items: any;
 
-  addToCart(item: any): void {
+  constructor(private readonly cartService: CartService) {}
 
+  addToCart(item: any): void {
+    this.cartService.addToCart(item);
   }
 }
