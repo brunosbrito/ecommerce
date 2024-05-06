a3222edfc08ef741d143a65fc4edf5ebdf45fc44
Bruno <brunobrito728@gmail.com>
2024-05-06 19:47:52 -0300

    created tests in admin

--- src/services/bcrypt.service.ts
+++ src/services/bcrypt.service.ts
@@ -5,7 +5,7 @@ import * as bcrypt from 'bcrypt';
 export class BcryptService {
   async hashPassword(password: string): Promise<string> {
     if (!password) {
-      throw new Error('Password is required to hash');
+      throw new Error('Senha e necessario para criar o hash');
     }
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
